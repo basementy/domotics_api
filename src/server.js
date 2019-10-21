caec67d843bb388e3adbf31cbc32a973de8721ad
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2019-10-21 16:01:27 -0300

    Setup the server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const cors = require('cors')
 
 const routes = require('./routes')
 
+mongoose.connect('mongodb+srv://biewxw:biewxw@personal-sysdr.mongodb.net/domotics?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log("\n-> Server working on port 3000")
+var port = 3000
+
+app.listen(port, () => {
+  console.log(`\n-> Server working on port ${port}`)
 })
