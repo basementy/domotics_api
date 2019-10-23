454432aca16b4664563cda628dd3ec25d87e3467
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2019-10-23 10:55:46 -0300

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
@@ -16,4 +16,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.PORT || 3000,)
+app.listen(process.env.PORT || 8080)
