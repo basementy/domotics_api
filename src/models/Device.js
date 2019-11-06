0ad41df8d6a5cc07c1c746db255b768b8e180256
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2019-11-06 16:03:16 -0300

    Created more fields for the model

--- src/models/Device.js
+++ src/models/Device.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const DeviceSchema = new mongoose.Schema({
   name: String,
-  status: Boolean
+  status: Boolean,
+  info: {
+    temperature: Number,
+    situation: String
+  }
 })
 
 module.exports = mongoose.model('Device', DeviceSchema)
